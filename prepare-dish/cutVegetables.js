34a4dadcb8fb3e05f7a53e79c71c787c0d351fc0
“NischithIsAFK” <nischith.212@gmail.com>
2024-09-18 15:46:49 +0530

    testing commit for pipeline

--- prepare-dish/cutVegetables.js
+++ prepare-dish/cutVegetables.js
@@ -2,7 +2,7 @@ exports.handler=async(event)=>{
   console.log("cutting veggies ");
   return{
     statusCode:200,
-    body:"Bought vegetables from the shop and cut vegetables finished"
+    body:"Bought vegetables from the shops and cut vegetables finished"
   }
   
 }
